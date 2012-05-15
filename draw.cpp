e824157c5de5c7bf5fcd2a95ff33fdd8e4feb6f7
Hendrik Radke <hermes9@web.de>
2012-05-15 10:48:37 +0200

    XML-Parser ausgebaut.

--- draw.cpp
+++ draw.cpp
@@ -25,56 +25,119 @@
 #include <magic.h>
 #include <Imlib2.h>
 #include <map>
+#include <vector>
 #include <string>
 #include <string.h>
 #include <iostream>
 
 using std::string;
-using std::map;
 
 #define BUFFSIZE        8192
 
 char Buff[BUFFSIZE];
 
-static void XMLCALL start(void *data, const char *el, const char **attr) {
+struct Color {
+    int r,g,b,a;
+    Color(int _r,int _g, int _b,int _a)
+	: r(_r), g(_g), b(_b), a(_a) {};
+    void use() {
+	imlib_context_set_color(r,g,b,a);
+    }
+};
+
+struct CFont {
+    CFont(string _name, string _size, Color _color)
+	: name(_name), size(_size), color(_color) {
+	imlib_font = imlib_load_font((name + "/" + size).c_str());
+    }
+    ~CFont() {
+	use();
+	printf("freeing font\n");
+	imlib_free_font();
+    }
+    string name;
+    string size;
+    Color color;
+    Imlib_Font imlib_font;
+    void use() {
+	imlib_context_set_font(imlib_font);
+	color.use();
+    }
+};
+
+std::vector<Color> colors;
+std::vector<CFont>  fonts;
+
+static void XMLCALL xml_start(void *data, const char *el, const char **attr) {
     int i;
-    map<string, string> args;
+    std::map<string, string> args;
     for (i = 0; attr[i]; i += 2) {
 	args[string(attr[i])] = string(attr[i+1]);
 	//printf(" %s='%s'", attr[i], attr[i + 1]);
     }
     if (strcmp(el, "ellipse") == 0) {
-	imlib_context_set_color(atoi(args["colorr"].c_str()),
-				atoi(args["colorg"].c_str()),
-				atoi(args["colorb"].c_str()),
-				255);
+	colors.back().use();
 	imlib_image_fill_ellipse(atoi(args["centerx"].c_str()),
 				 atoi(args["centery"].c_str()),
 				 atoi(args["radiusb"].c_str())-4,
 				 atoi(args["radiusa"].c_str())-4);
-	imlib_context_set_color(0, 0, 0, 255);
+	fonts.back().use();
 	imlib_text_draw(atoi(args["centerx"].c_str()),atoi(args["centery"].c_str())-6, "bla");
+    } else if (strcmp(el, "rectangle") == 0) {
+	// todo
+    } else if (strcmp(el, "font") == 0) {
+	// todo
+	CFont fcurrent = CFont(args["name"],
+			       args["size"],
+			       Color(atoi(args["colorr"].c_str()),
+				     atoi(args["colorg"].c_str()),
+				     atoi(args["colorb"].c_str()),
+				     255)
+	    );
+	fonts.push_back(fcurrent);
+    } else if (strcmp(el, "bgcolor") == 0) {
+	colors.push_back(Color(atoi(args["r"].c_str()),
+			       atoi(args["g"].c_str()),
+			       atoi(args["b"].c_str()),
+			       atoi(args["a"].c_str())));
+    } else if (strcmp(el, "comic") == 0) {
+	Imlib_Image image = imlib_load_image("oots0123.gif");
+	imlib_context_set_image(image);
     }
     //printf("%s", el);
     //printf("\n");
 }
 
-static void XMLCALL end(void *data, const char *el) {
+static void XMLCALL xml_end(void *data, const char *el) {
+    if (strcmp(el, "bgcolor") == 0) {
+	colors.pop_back();
+    } else if (strcmp(el, "font") == 0) {
+	fonts.pop_back();
+    } else if (strcmp(el, "comic") == 0) {
+	Imlib_Load_Error err;
+	imlib_image_set_format("png");
+	imlib_save_image_with_error_return("de0123.png", &err);
+	if (err != 0) {
+	    printf("error %d saving image.\n", err);
+	}
+	imlib_free_image();
+    }
+}
+
+static void XMLCALL xml_data(void *userData, const XML_Char *s, int len) {
+    // todo
 }
 
 int main(int argc, char *argv[]) {
-    XML_Parser p = XML_ParserCreate(NULL);
-    if (! p) {
+    XML_Parser parser = XML_ParserCreate(NULL);
+    if (! parser) {
 	fprintf(stderr, "Couldn't allocate memory for parser\n");
 	exit(-1);
     }
 
-    XML_SetElementHandler(p, start, end);
-
-    Imlib_Image image = imlib_load_image("oots0123.gif");
-    imlib_context_set_image(image);
-    Imlib_Font font = imlib_load_font("FreeSans/8");
-    imlib_context_set_font(font);
+    XML_SetElementHandler(parser, xml_start, xml_end);
+    XML_SetCharacterDataHandler(parser, xml_data);
+    
 
     while(true) {
 	int done;
@@ -87,22 +150,15 @@ int main(int argc, char *argv[]) {
 	}
 	done = feof(stdin);
 
-	if (XML_Parse(p, Buff, len, done) == XML_STATUS_ERROR) {
+	if (XML_Parse(parser, Buff, len, done) == XML_STATUS_ERROR) {
 	    fprintf(stderr, "Parse error at line %d: %s\n",
-		    XML_GetCurrentLineNumber(p),
-		    XML_ErrorString(XML_GetErrorCode(p)));
+		    XML_GetCurrentLineNumber(parser),
+		    XML_ErrorString(XML_GetErrorCode(parser)));
 	    exit(-1);
 	}
 
 	if (done) break;
     }
-    Imlib_Load_Error err;
-    imlib_image_set_format("png");
-    imlib_save_image_with_error_return("de0123.png", &err);
-    printf("saving yields error %d/%d\n", err, IMLIB_LOAD_ERROR_NO_LOADER_FOR_FILE_FORMAT);
-    imlib_free_font();
-    imlib_free_image();
-
-    XML_ParserFree(p);
+    XML_ParserFree(parser);
     return 0;
 }
