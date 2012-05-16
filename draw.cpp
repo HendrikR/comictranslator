7c7e093a6bef44181ef51814de212b056ce9274d
Hendrik Radke <hermes9@web.de>
2012-05-16 23:21:30 +0200

    Verbesserungen am Text-Aufteilungs-Algorithmus, Behandlung von ~ als non-breakable space.

--- draw.cpp
+++ draw.cpp
@@ -227,25 +227,31 @@ int main(int argc, char *argv[]) {
 }
 
 int ellipseWidth(float a, float b, float y) {
-    // OBdA sei b <= a.
-    assert(b <= a);
-    // Ellipsen-Gleichung b^2x^2 + a^2y^2 = a^2b^2, umgestellt nach x
-    //return static_cast<int>(round(sqrt(pow(b,2)-pow(a,2))/2 + sqrt( pow(a,2)*(pow(b,2)-pow(y,2)) / pow(b,2) )))*2;
+    assert(b <= a);    // OBdA sei b <= a.
     // Ellipsen-Gleichung x^2/a^2 - y^2/b^2 = 1, umgestellt nach x
     return static_cast<int>(round(sqrt((1.0 - (y*y)/(b*b)) * (a*a))*2.0));
 }
 
-string drawTextLine(int x0, int y0, string text, int maxwidth) {
+string replace_all(string str, string from, string to) {
+    string::size_type pos = 0;
+    while( (pos = str.find(from, pos)) != string::npos) {
+	str.replace(pos, from.size(), to);
+	pos++;
+    }
+    return str;
+}
+
+string drawTextLine(int x0, int y0, string text, int maxwidth, float rel_height) {
     string prefix = "", oldprefix = "";
     int height, width = 0;
     while (width < maxwidth && oldprefix != text) {
 	oldprefix = prefix;
 	// Find the next word boundary
 	size_t len = prefix.length();
-	size_t word_idx = text.find_first_of(" \t\n", len);
+	size_t word_idx = text.find(" ", len+1);
 	
 	if (word_idx != string::npos) {
-	    prefix = text.substr(0, word_idx+1);
+	    prefix = text.substr(0, word_idx);
 	} else {
 	    prefix = text;
 	}
@@ -253,9 +259,12 @@ string drawTextLine(int x0, int y0, string text, int maxwidth) {
     }
     
     imlib_get_text_size(oldprefix.c_str(), &width, &height);
-    imlib_text_draw(x0+(maxwidth-width)/2, y0,  oldprefix.c_str());
+    imlib_text_draw(x0+(maxwidth-width)/2 + 2,
+		    y0 - height/2 - static_cast<int>(round(1.1*rel_height*(height/2.0))),
+		    replace_all(oldprefix, "~", " ").c_str());
     //imlib_image_draw_line(x0, y0, x0+maxwidth, y0, 0);
-    return text.substr(oldprefix.length());
+    if (oldprefix == text) return "";
+    else                   return text.substr(oldprefix.length()+1);
 }
 
 void Bubble::draw() {
@@ -270,17 +279,28 @@ void Bubble::draw() {
 	fonts.back().use();
 	int len      = text.length();
 	int fontsize = static_cast<int>(round(atof(fonts.back().size.c_str())));
-	int width    = 0;
-	int height   = -radiusy + fontsize/2;
+	// Wähle die Höhe der 1. Zeile so, dass das 1. Wort gerade hineinpasst.
+	int width, height;
+	int first_idx     = text.find(' ');
+	string first_word = (first_idx != string::npos) ? text.substr(0,first_idx) : text;
+	imlib_get_text_size(first_word.c_str(), &width, &height);
+	height = -radiusy;
+	do {
+	    height++;
+	    //printf("%s of width %d does not fit into width %d, increase h to %d\n",
+	    //first_word.c_str(), width, ellipseWidth(radiusx, radiusy, abs(height)), height);
+	} while (ellipseWidth(radiusx, radiusy, abs(height)) < width && height <= 0);
+	height += fontsize/3;
+	// Schreibe den Text nacheinander in die Zeilen.
 	string rest  = text;
 	while(rest.length() > 0) {
-	    if (height > radiusy/2 - fontsize) {
-		printf("warning: text does not fit, aborting: %s.\n", text.c_str());
+	    if (height > radiusy-2) {
+		fprintf(stderr, "warning: text does not fit, aborting: %s.\n", text.c_str());
 		break;
 	    }
 	    width = ellipseWidth(radiusx, radiusy, abs(height));
-	    rest = drawTextLine(centerx-width/2, centery+height, rest, width);
-	    height += fontsize + fontsize/4;
+	    rest = drawTextLine(centerx-width/2, centery+height, rest, width, height/(float)radiusx);
+	    height += fontsize*1.7;
 	}
     } else if (shape == "rectangle") {
 	// todo
